4fa911ceb76ca4c75b4e4fe8d44276729b468b46
swekhalidahmed <abdullah.khaled@jouleslabs.com>
2021-08-22 20:04:16 +0600

    create folder structure

---
+++ indea-manager.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Idea Manager
+ *
+ * @package           PluginPackage
+ * @author            Khalid Ahmed
+ * @copyright         2021 Khalid Ahmed
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Idea manager
+ * Plugin URI:        https://github.com/Khalidwebmail/idea-manager
+ * Description:       Manage idea management system that will help to visualize the idea requests currently in the queue along with status.
+ * Version:           1.0.0
+ * Requires at least: 7.0
+ * Author:            Khalid Ahmed
+ * Author URI:        https://github.com/Khalidwebmail
+ * Text Domain:       idea-manager
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
